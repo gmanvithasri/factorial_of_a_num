eb21fd5c9589e1845b7404d553b6945aad19c6b6
gmanvithasri <108047448+gmanvithasri@users.noreply.github.com>
2024-01-08 20:49:23 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int factorial (int num)
-{
-    int i;
-    int result = 1;
-    if (num<0)
+long long int factorial(int num) {
+    if (num < 0) {
         return -1;
-    for (i=1;i<=num;i++)
-    {
-        result = result*i;
+    }
+
+    long long int result = 1;
+    for (int i = 1; i <= num; i++) {
+        result *= i;
     }
     return result;
 }
 
-int main()
-{
+int main() {
     int myNum;
-    printf("Please insert a number:");
-    scanf("%d", &myNum);
-    printf("Factorial(%d) = %d\n", myNum, factorial(myNum));
+    printf("Please insert a non-negative integer: ");
+    if (scanf("%d", &myNum) != 1) {
+        printf("Invalid input! Please enter an integer.\n");
+        return 1;
+    }
+
+    if (myNum < 0) {
+        printf("Factorial of a negative number is not defined.\n");
+        return 1;
+    }
+
+    long long int result = factorial(myNum);
+    if (result == -1) {
+        printf("Factorial of a negative number is not defined.\n");
+    } else {
+        printf("Factorial(%d) = %lld\n", myNum, result);
+    }
 
     return 0;
 }
